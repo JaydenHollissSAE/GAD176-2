3335a6d1169f4011b2bb11c6216bd61e22d7dd24
ThomasJenner1999 <1032861@student.sae.edu.au>
2024-07-25 11:56:27 +1000

    start of character movement sprint
    began adding code to allow players to sprint if key is help down, implemented ridged body and physics to player controller.

--- Assets/S_CharacterMovement.cs
+++ Assets/S_CharacterMovement.cs
@@ -7,11 +7,19 @@ public class CharacterMovementScript : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-    public float speed = 6f;
+    public float walkSpeed = 6f;
+    public float sprintSpeed = 12f;
+    public float jumpSpeed;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
 
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +28,11 @@ public class CharacterMovementScript : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //grounded check
+        grounded = Physics.Raycast(transform.position, Vector3.down, )
+
+        
 
         //character orientation
         if (direction.magnitude >= 0.1f)
@@ -30,6 +43,14 @@ public class CharacterMovementScript : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+        }
+
+    }
+    private void StateHandler()
+    {
+        if grounded && Input.GetKey(KeyCode.LeftShift))
+        {
 
         }
     }
