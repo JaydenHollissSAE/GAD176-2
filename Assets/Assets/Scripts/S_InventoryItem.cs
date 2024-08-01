80d3d4db88b4f1aa927778a7fe7900af6ca8cb86
ThomasJenner1999 <1032861@student.sae.edu.au>
2024-08-01 11:56:10 +1000

    Inventory system base work done
    had to bring over the character movement stuff for testing purposes, got most of the UI elements working for the inventory system, so you can open and close it

---
+++ Assets/Assets/Scripts/S_InventoryItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Item/Create new Item")]
+
+public class S_Inventory : MonoBehaviour
+{
+    public int id;
+    public string itemName;
+    public int value;
+    public Sprite image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
