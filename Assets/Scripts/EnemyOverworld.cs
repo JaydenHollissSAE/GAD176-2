407409b4d1725e7362692a158df7784a62b20bbe
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-08-01 13:02:01 +1000

    Added namespace and start of navmesh path finding

--- Assets/Scripts/EnemyOverworld.cs
+++ Assets/Scripts/EnemyOverworld.cs
@@ -1,117 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyOverworld : MonoBehaviour
+namespace GAD176.ProjectRPG
 {
-    private GameObject player;
-    public GameObject[] players;
-    public bool multiplayer;
-    [SerializeField] private bool edgeGuard;
-    [SerializeField] private float maxDropDistance = 2.0f;
-    public float detectDistance = 10f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyOverworld : MonoBehaviour
     {
-        if (multiplayer)
-        {
-            players = GameObject.FindGameObjectsWithTag("Player");
-        }
-        else
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (multiplayer)
-        {
-            GetClosestPlayer();
-        }
-        if (edgeGuard) 
-        {
-            EdgeGuard();
-        }
-        if ((Vector3.Distance(player.transform.position, transform.position)) < detectDistance)
-        {
-            MoveTowardsPlayer();
-
-        }
+        private GameObject player;
+        public GameObject[] players;
+        public bool multiplayer;
+        [SerializeField] private bool edgeGuard;
+        [SerializeField] private float maxDropDistance = 2.0f;
+        public float detectDistance = 10f;
+        [SerializeField] private NavMeshAgent agent;
 
 
-    }
-    private void GetClosestPlayer()
-    {
-        float clostest = 10000f;
-        for (int i = 0; i < players.Length; i++) 
+        // Start is called before the first frame update
+        void Start()
         {
-            float distance = (Vector3.Distance(players[i].transform.position, transform.position));
-            if (distance < clostest) 
-            { 
-                clostest = distance;
-                player = players[i];
+            if (multiplayer)
+            {
+                players = GameObject.FindGameObjectsWithTag("Player");
+            }
+            else
+            {
+                player = GameObject.FindGameObjectWithTag("Player");
             }
         }
-    }
-    private void MoveTowardsPlayer()
-    {
-        transform.LookAt(player.transform.position);
-        RaycastHit hitForward;
-        RaycastHit hitLeft;
-        RaycastHit hitRight;
-        RaycastHit hitBack;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitForward, Mathf.Infinity))
+
+        // Update is called once per frame
+        void Update()
         {
-            if (!(hitForward.distance < 1.5f))
+            if (multiplayer)
             {
-                Debug.Log(hitForward.transform.gameObject.name);
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 5.5f);
+                GetClosestPlayer();
+            }
+            if (edgeGuard)
+            {
+                EdgeGuard();
+            }
+            if ((Vector3.Distance(player.transform.position, transform.position)) < detectDistance)
+            {
+                MoveTowardsPlayer();
+
+            }
+
+
+        }
+        private void GetClosestPlayer()
+        {
+            float clostest = 10000f;
+            for (int i = 0; i < players.Length; i++)
+            {
+                float distance = (Vector3.Distance(players[i].transform.position, transform.position));
+                if (distance < clostest)
+                {
+                    clostest = distance;
+                    player = players[i];
+                }
             }
         }
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hitLeft, Mathf.Infinity);
-        if (!(hitLeft.distance < 1.5f))
+        private void MoveTowardsPlayer()
         {
-            Debug.Log(hitLeft.transform.gameObject.name);
-            transform.position = Vector3.MoveTowards(transform.position, Vector3.left, Time.deltaTime * 5.5f);
+            agent.SetDestination(player.transform.position);
+            //transform.LookAt(player.transform.position);
+            //RaycastHit hitForward;
+            //RaycastHit hitLeft;
+            //RaycastHit hitRight;
+            //RaycastHit hitBack;
+            //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitForward, Mathf.Infinity))
+            //{
+            //    if (!(hitForward.distance < 1.5f))
+            //    {
+            //        Debug.Log(hitForward.transform.gameObject.name);
+            //        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 5.5f);
+            //    }
+            //}
+            //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hitLeft, Mathf.Infinity);
+            //if (!(hitLeft.distance < 1.5f))
+            //{
+            //    Debug.Log(hitLeft.transform.gameObject.name);
+            //    transform.position = Vector3.MoveTowards(transform.position, Vector3.left, Time.deltaTime * 5.5f);
+            //}
+            //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hitRight, Mathf.Infinity);
+            //if (!(hitRight.distance < 1.5f))
+            //{
+            //    Debug.Log(hitRight.transform.gameObject.name);
+            //    transform.position = Vector3.MoveTowards(transform.position, Vector3.right, Time.deltaTime * 5.5f);
+            //}
+            //Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hitBack, Mathf.Infinity);
+            //if (!(hitBack.distance < 1.5f))
+            //{
+            //    Debug.Log(hitBack.transform.gameObject.name);
+            //    transform.position = Vector3.MoveTowards(transform.position, Vector3.back, Time.deltaTime * 5.5f);
+            //}
         }
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hitRight, Mathf.Infinity);
-        if (!(hitRight.distance < 1.5f))
+        private void EdgeGuard()
         {
-            Debug.Log(hitRight.transform.gameObject.name);
-            transform.position = Vector3.MoveTowards(transform.position, Vector3.right, Time.deltaTime * 5.5f);
-        }
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hitBack, Mathf.Infinity);
-        if (!(hitBack.distance < 1.5f))
-        {
-            Debug.Log(hitBack.transform.gameObject.name);
-            transform.position = Vector3.MoveTowards(transform.position, Vector3.back, Time.deltaTime * 5.5f);
-        }
-    }
-    private void EdgeGuard()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
-        {
-            //Debug.Log(hit.distance);
-            if (hit.distance < maxDropDistance)
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
             {
-                Debug.Log("This is an acceptable height to walk down from");
+                //Debug.Log(hit.distance);
+                if (hit.distance < maxDropDistance)
+                {
+                    Debug.Log("This is an acceptable height to walk down from");
+                }
+                else
+                {
+                    Debug.Log("NOPE! NOPE!! NOPE!!! NOPE! NOPE!! NOPE!!!");
+                }
+
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+                //Debug.Log("I am working guys, trust me");
             }
             else
             {
                 Debug.Log("NOPE! NOPE!! NOPE!!! NOPE! NOPE!! NOPE!!!");
             }
-            
-            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-            //Debug.Log("I am working guys, trust me");
-        }
-        else
-        {
-            Debug.Log("NOPE! NOPE!! NOPE!!! NOPE! NOPE!! NOPE!!!");
-        }
 
+        }
     }
 }
