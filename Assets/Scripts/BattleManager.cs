b7a54eee32607e2f29aff67813bbbf4a5f8658ee
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-08-01 12:59:37 +1000

    Started battle initiation

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleManager : MonoBehaviour
+namespace GAD176.ProjectRPG
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BattleManager : MonoBehaviour
     {
-        
-    }
+        public List<GameObject> inBattleEnemies = new List<GameObject>();
+        private GameManager gameManager;
+        private Enemy enemy;
+        private int enemyCount;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (gameManager.randomisedEnemies == true)
+            {
+                enemyCount = Random.Range(1, gameManager.maxEnemies);
+            }
+            else
+            {
+                enemy = enemy.enemyCount;
+            }
+            for (int i = 0; i < enemyCount; i++)
+            {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
