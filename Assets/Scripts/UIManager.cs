68a99419579fcf234f29ca1e42ba96e6a3e37365
Sarale09 <alejandrasarmiento0998@gmail.com>
2024-08-07 21:40:52 +1000

    Basic Layout of Battle UI Completed

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void InventoryButton()
+    {
+        //function that is activated with the click of the inventory button
+        Debug.Log("Display Inventory");
+    }
+}
