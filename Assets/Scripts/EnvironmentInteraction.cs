504d71ab01fdd6e0b28d7d1f12d0dcb020b7f66d
miloeck <159411519+miloeck@users.noreply.github.com>
2024-08-08 11:23:44 +1000

    check for object in front of player
    hypothetically works with the right inputs. user can add in changes to items' shaders as desired.

---
+++ Assets/Scripts/EnvironmentInteraction.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+
+namespace GAD176.ProjectRPG
+{
+    public class EnvironmentInteraction : MonoBehaviour
+    {
+        // assumes a class exists for environmental items
+        [SerializeField] private EnvironmentItems environmentItems;
+        // assumes a player class exists
+        [SerializeField] private Player player;
+        [SerializeField] private GameObject lastInteractable;
+
+
+        private void Update()
+        {
+            CheckObjectInFront();
+        }
+        private void Interact(GameObject item)
+        {
+
+        }
+
+        private void CheckObjectInFront()
+        {
+            RaycastHit hit;
+            // assumes a player camera exists; looks for an object 5m ahead
+            Physics.Raycast(Player.camera.transform.position, Player.camera.transform.forward, out hit, 5f, environmentItems);
+            GameObject hitObject = hit.transform.gameObject;
+            if (!hit.collider)
+            {
+                lastInteractable = null;
+            }
+            if (lastInteractable != hitObject)
+            {
+                MakeUninteractable(lastInteractable);
+                lastInteractable = hitObject;
+            }
+            MakeInteractable(hitObject);
+        }
+
+        private void MakeInteractable(GameObject item)
+        {
+            if (!item) { return; }
+            // logic for changing item's shaders, etc. to highlight interactability...
+        }
+
+        private void MakeUninteractable(GameObject item)
+        {
+            if (!item) { return; }
+            // opposite of the other function obviously.
+        }
+    }
+}
