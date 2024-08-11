abf15d4df57f4ce80b7535405cfe3548e7eea7d0
Sarale09 <alejandrasarmiento0998@gmail.com>
2024-08-11 18:50:57 +1000

    Stats Panel Displaying Info When Hovering Over Button

--- Assets/Scripts/UI/UIRaycaster.cs
+++ Assets/Scripts/UI/UIRaycaster.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class UIRaycaster : MonoBehaviour
 {
     GraphicRaycaster Ray;
     PointerEventData CursorData;
-    EventSystem EventSystem;
+    public TMP_Text SkillStatDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
         //Fetch Raycaster and Event System
         Ray = GetComponent<GraphicRaycaster>();
-        EventSystem = GetComponent<EventSystem>();
 
     }
 
@@ -30,12 +31,15 @@ public class UIRaycaster : MonoBehaviour
             //Debug.Log(result.gameObject.name);
             if (result.gameObject.name == "SkillButton1")
             {
+                SkillStatDisplay.text = ("Displaying Stats 1");
                 Debug.Log("Skill1Hit");
             } else if (result.gameObject.name == "SkillButton2")
             {
+                SkillStatDisplay.text = ("Displaying Stats 2");
                 Debug.Log("Skill2Hit");
             } else if (result.gameObject.name == "SkillButton3")
             {
+                SkillStatDisplay.text = ("Displaying Stats 3");
                 Debug.Log("Skill3Hit");
             }
         }
