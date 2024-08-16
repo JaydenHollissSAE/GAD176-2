a42f2a9b73fa3538ad90b0abde2523a7180330bf
Sarale09 <alejandrasarmiento0998@gmail.com>
2024-08-16 22:26:57 +1000

    Added Heal Skill for enemies

--- Assets/Scripts/EnemySkills.cs
+++ Assets/Scripts/EnemySkills.cs
@@ -6,23 +6,35 @@ namespace Enemy
 {
     public class EnemySkills : IAttack
     {
-        public float PhysicalAttack(float PhyAttack, float AttackMod)
+        public float PhysicalAttack(float phyAttack, float attackMod)
         {
             //Get max value of physical attack & attack modifier from enemy's stats 
             //return value of random range with max of physical attack and min of 1
             //(So attack success can vary and cause more or less damage)
             //then multiplied by the enemy's modifier
-            float damage = Mathf.Round( Random.Range(1f, PhyAttack) *AttackMod);
+
+            float damage = Mathf.Round( Random.Range(1f, phyAttack) *attackMod);
+
+            //return the amount of damage done, battle manager to allocate this damage to respective target
             return damage;  
         }
-        public float MagicAttack(float MagAttack, float AttackMod)
+        public float MagicAttack(float magAttack, float attackMod)
         {
             //Get max value of magical attack & attack modifier from enemy's stats 
-            //return value of random range with max of physical attack and min of 1
+            //return value of random range with max of magical attack and min of 1
             //(So attack success can vary and cause more or less damage)
             //then multiplied by the enemy's modifier
-            float damage = Mathf.Round(Random.Range(1f, MagAttack) * AttackMod);
+
+            float damage = Mathf.Round(Random.Range(1f, magAttack) * attackMod);
+
+            //return the amount of damage done, battle manager to allocate this damage to respective target
             return damage;
+        }
+        public void Heal(int maxHeal, ref int currentHealth )
+        {
+            //calculate amount to heal and add this to the current health of the enemy (Itself)
+            int healing = Random.Range( 0, maxHeal );
+            currentHealth += healing;
         }
 
     }
